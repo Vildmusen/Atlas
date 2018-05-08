6f149807ab7e74bc085241a145806d100d0e1950
Vildmusen <viktorvilmusenaho@hotmail.com>
2018-05-08 14:45:56 +0200

    added style.css, script.js

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+window.onload = function test() {
+    alert("det funkar!");
+};
--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<DOCTYPE html>
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <meta charset="utf-8" />
+        <title>Projektet my dude</title>
+
+        <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css" />
+        <script src="assets/js/script.js"></script>
+    </head>
+    <body>
+       <p> hello there </p>
+    </body>
+</html>
