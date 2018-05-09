0dbd488e81ac092eb18f0163cb9ee736e8dd7159
elingosch <elin.gosch@gmail.com>
2018-05-09 14:28:19 +0200

    Fixade lite till hehe

--- index.php
+++ index.php
@@ -9,18 +9,13 @@
         <script src="assets/js/script.js"></script>
     </head>
     <body>
-<<<<<<< HEAD
         <div id="header">
             <h1>ATLAS</h1>
             <p> HEJEEEE </p>
         </div>
      
-=======
-       <p> HEJEEEE </p>
-
        <div id="main">
             test
        </div>
->>>>>>> 7e3efa29b416fe102bdff6ad25a7d717f98e7b18
     </body>
 </html>
