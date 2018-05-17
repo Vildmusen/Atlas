101b0f396c2f45a3190933de1920c198564712e2
Vildmusen <viktorvilmusenaho@hotmail.com>
2018-05-17 13:50:34 +0200

    started implementing båtstrap

--- index.php
+++ index.php
@@ -2,19 +2,59 @@
 <html>
     <head>
 
-        <meta charset="utf-8" />
-        <title>Projektet my dude</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css" />
-        <script src="assets/js/script.js"></script>
+        <title>Projektet my dude</title>
+    
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/style.css">
     </head>
     <body>
-        <div id="header">
-            <h1>ATLAS</h1>
-        </div>
-     
-        <div id="main">
-            test
-        </div>
+
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    
+            <a class="navbar-brand" href="#">Atlas</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Hem <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="main.php">Utforska</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="omoss.php">Om oss</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+
+        <main role="main" class="container" id="wrapper">
+
+            <div class="container" id="welcome_box">
+                <h2>Välkommen!</h2>
+                <p>Välkommen till Atlas, din plattform för att utforska Sverige! Navigera sidan anonymt eller registrera dig och logga in för att ta del av samtliga funktioner.</p>
+            </div>
+
+            <div class="conatiner" id="link_box">
+                <a href="login.php"><button class="btn btn-block">Logga in</button></a>
+                <a href="register.php"><button class="btn btn-block">Registrera</button></a>
+                <a href="main.php"><button class="btn btn-block" id="center_button">Utforska</button></a>
+            </div>
+        </main>
+    
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+        
     </body>
 </html>
--- main.php
+++ main.php
@@ -14,26 +14,44 @@
     <head>
 
         <meta charset="utf-8">
-        <title>Projektet my dude</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <link rel="stylesheet" type="text/css" media="screen" href="assets/css/main.css" />
+        <title>Projektet my dude</title>
+    
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        
+        <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css" />
         <script src="assets/js/script.js"></script>
     </head>
     <body>
 
-        <div id="header">
-            <div id="logo">
-                <h1>ATLAS</h1>
-            </div>
-            <div id="user_info">
-                <div id="user_text"><h3>Välkommen, användare!</h3></div>
-                <div id="user_icon"></div></div>
-            </div>
-        </div>
+        
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    
+            <a class="navbar-brand" href="#">Atlas</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div id="main">
-            
-        <div id="location">
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Hem</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="main.php">Utforska<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="omoss.php">Om oss</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+
 
                 <h2 style="color: black;">Uppsala</h2>
 
@@ -72,5 +90,9 @@
                 ?>
             </div>
        </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+        
     </body>
 </html>
