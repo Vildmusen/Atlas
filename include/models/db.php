f340228802068dcce432ebafc9c47d1d7a462ae0
Vildmusen <viktorvilmusenaho@hotmail.com>
2018-05-25 12:05:07 +0200

    voting system implemented

--- include/models/db.php
+++ include/models/db.php
@@ -14,6 +14,7 @@ function connect(){
     $connection->set_charset("utf8");
     return $connection;
 }
+
 function makeUser($salt){
     $connection = connect();
     $hashed_pass = sha1($_POST["pass"].$salt);
@@ -27,11 +28,13 @@ function makeUser($salt){
     $stmt = $connection->query($sql);
     return $stmt;
 }
+
 function getcomments(){
     $sql = "SELECT * FROM post";
     $result = connect()->query($sql);
     return $result;
 }
+
 function getuser($id=""){
     if ($id == ""){
         $sql = "SELECT * FROM user";
@@ -43,6 +46,7 @@ function getuser($id=""){
         return $result->fetch_assoc();
     }
 }
+
 function getpost($location){
     $query = "SELECT * FROM post WHERE l_id='$location'";
     return connect()->query($query);
@@ -64,9 +68,11 @@ function getcity($location=""){
         return $result->fetch_assoc();
     }
 }
+
 function getallcities(){
     return connect()->query("SELECT * from location");
 }
+
 function createPost($tempID = 0){
     $connection = connect();
     $title = mysqli_real_escape_string($connection, $_POST['title']);
@@ -80,15 +86,16 @@ function createPost($tempID = 0){
     $stmt = $connection->query($sql);
     return $stmt;
 }
+
 function updatePost($tempID){
     $sql = "UPDATE post SET parent_id= '$tempID' WHERE p_id= '$tempID'";
     return connect()->query($sql);
 }
+
 function getMaxId(){
     $sql = "SELECT MAX(p_id) AS id FROM post";
     return connect()->query($sql);
 }
-
 
 function getReason(){
     $sql = "SELECT reason FROM reason";
@@ -104,9 +111,46 @@ function getTotalComments($parent_id) {
 }
 
 function getPostRating($post_id) {
-    $sql = "SELECT post_rating FROM rating WHERE p_id = '$post_id'";
+    $sql = "SELECT rating FROM post WHERE p_id = '$post_id'";
     $result = connect()->query($sql);
     $postRating = $result->fetch_assoc();
-    return $postRating['post_rating'];
+    return $postRating['rating'];
+}
+
+function ratePost($post_id, $val) {
+    $newrating = getPostRating($post_id) + $val;
+    $sql = "UPDATE post SET rating = '$newrating' WHERE p_id = '$post_id'";
+    connect()->query($sql);
+}
+
+function getPostUserRating($u_id, $post_id){
+    $sql = "SELECT vote_value FROM rating WHERE u_id = '$u_id' and p_id = '$post_id'";
+    return connect()->query($sql);
+}
+
+function allowedToVote($u_id, $post_id, $val){
+    $result = getPostUserRating($u_id, $post_id);
+    if($result->fetch_assoc() != null){
+        $ratingArr = $result->fetch_assoc();
+        if($val != $ratingArr['vote_value']){
+            $sql = "UPDATE rating SET vote_value = '$val' WHERE u_id = '$u_id' AND p_id = '$post_id'";
+            connect()->query($sql);
+            return "changed";
+        }
+        return "false";
+    }
+    return "new";
+}
+
+function saveVote($u_id, $post_id, $post_rating, $val){
+    $result = getPostUserRating($u_id, $post_id);
+    if($result->fetch_assoc() != null){
+        $sql = "UPDATE rating SET vote_value = '$val' WHERE (p_id = '$post_id' AND u_id = '$u_id')";
+        connect()->query($sql);
+    } else {
+        $sql = "INSERT INTO rating (u_id, p_id, post_rating, vote_value)
+        VALUES ('$u_id', '$post_id', '$post_rating', '$val')";
+        connect()->query($sql);
+    }
 }
 ?>
