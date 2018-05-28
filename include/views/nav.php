c625af0a78d013fd8bcc1689c8c61f66c1697e37
Vildmusen <viktorvilmusenaho@hotmail.com>
2018-05-28 09:26:28 +0200

    länkändring

--- include/views/nav.php
+++ include/views/nav.php
@@ -8,7 +8,7 @@
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
 
-    <a class="navbar-brand" href="#">Atlas</a>
+    <a class="navbar-brand" href="main.php">Atlas</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation" onclick="show_hide_drop('location_dropdown');">
         <span class="navbar-toggler-icon"></span>
     </button>
